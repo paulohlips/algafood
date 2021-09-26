5cd672178dd2f517368b02b4efb89ea73e243678
paulohlips <paulohfcmendes@gmail.com>
2021-09-26 09:17:51 -0300

    third module (JPA) completed

---
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaFormaPagamentoMain.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApplication;
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
+
+
+public class ConsultaFormaPagamentoMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		FormaPagamentoRepository formaPagamentoRepository = applicationContext.getBean(FormaPagamentoRepository.class);
+		
+		List<FormaPagamento> formaPagamentos = formaPagamentoRepository.listar();
+		
+		for (FormaPagamento formaPagamento : formaPagamentos) {
+			System.out.println(formaPagamento.getDescricao());
+		}
+	}
+	
+}
